7870bfe9eec742f43d222685591babe450f39b28
yuan <yrw@sdec.com.cn>
2019-11-12 08:23:03 +0800

    M

--- main.c
+++ main.c
@@ -146,7 +146,7 @@ void init_USART2(void) {
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
